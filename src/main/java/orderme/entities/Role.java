2ef87b18fd94539c4212f3f2cc109895e6214817
Martín Seijo <martinseijoperez@gmail.com>
2024-10-16 20:56:04 +0200

    Se incorpora entidades y repositorios de user y role

---
+++ src/main/java/orderme/entities/Role.java
@@ -0,0 +1,20 @@
+package orderme.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+}
---
+++ src/main/java/orderme/entities/User.java
@@ -0,0 +1,47 @@
+package orderme.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Username is mandatory")
+    private String username;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    @Column(nullable = true)
+    private String phone;
+
+    @Column(nullable = false)
+    private boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+}
