9d86fb58942fc41a33db69ae90e9a3870338fd77
Martín Seijo <martinseijoperez@gmail.com>
2024-11-02 12:45:37 +0100

    Implementación de token

--- src/main/java/orderme/service/impl/JwtServiceImpl.java
+++ src/main/java/orderme/service/impl/JwtServiceImpl.java
@@ -2,18 +2,22 @@ package orderme.service.impl;
 
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import orderme.entities.User;
 import orderme.service.JwtService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 
 @Service
@@ -25,6 +29,18 @@ public class JwtServiceImpl implements JwtService {
     @Override
     public String getToken(User user) {
         return getToken(new HashMap<>(), user);
+    }
+
+    @Override
+    public String getUsernameFromToken(String token) {
+        return getClaim(token, Claims::getSubject);
+    }
+
+    @Override
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
     private String getToken(Map<String, Object> extraClaims, User user) {
@@ -40,5 +56,22 @@ public class JwtServiceImpl implements JwtService {
     private Key getKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private Claims getAllClaims(String token){
+        return Jwts.parser().verifyWith((SecretKey) getKey()).build().parseSignedClaims(token).getPayload();
+    }
+
+    public <T> T getClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Date getExpiration(String token){
+        return getClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token){
+        return getExpiration(token).before(new Date());
     }
 }
